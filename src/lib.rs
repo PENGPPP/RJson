126851fb8be83750c124038b4ad4c78f57761301
YANGJIWEI1992 <yangjiwei@hinterstellar.com>
2020-10-12 20:21:52 +0800

    rjson by TDD

---
+++ src/lib.rs
@@ -0,0 +1,162 @@
+mod rjson;
+
+#[cfg(test)]
+mod tests {
+
+    use super::rjson::*;
+
+    macro_rules! assert_eq_r {
+        ($x:expr, $y:pat) => {
+            match $x {
+                $y => (),
+                _ => panic!("error!!"),
+            }
+        };
+    }
+
+    macro_rules! assert_neq_r {
+        ($x:expr, $y:pat) => {
+            match $x {
+                $y => panic!("error!!"),
+                _ => (),
+            }
+        };
+    }
+
+    macro_rules! assert_eq_n_r {
+        ($x:expr, $y:expr, $z:expr) => {
+            match $x {
+                Ok(PareseResult {
+                    json,
+                    json_struct: JsonStruct::Number(num),
+                }) if num == $y && json == $z => (),
+
+                _ => panic!("parse num error!!"),
+            }
+        };
+    }
+
+    #[test]
+    fn test_parse() {
+        assert_eq_r!(parse("null"), Ok(JsonStruct::Null));
+        assert_eq_r!(parse("false"), Ok(JsonStruct::Boolean(false)));
+        assert_neq_r!(parse("false"), Ok(JsonStruct::Boolean(true)));
+        assert_eq_r!(parse("true"), Ok(JsonStruct::Boolean(true)));
+        assert_neq_r!(parse("true"), Ok(JsonStruct::Boolean(false)));
+
+        assert_eq_r!(parse("tru"), Err(ParseError::ValueError));
+        assert_eq_r!(parse("nul"), Err(ParseError::ValueError));
+        assert_eq_r!(parse("f"), Err(ParseError::ValueError));
+    }
+
+    #[test]
+    fn test_white_space_skip() {
+        assert_eq_r!(skip_white_space("   "), Ok(""));
+        assert_eq_r!(skip_white_space(" 1 "), Ok("1 "));
+        assert_eq_r!(skip_white_space("1   "), Ok("1   "));
+        assert_eq_r!(skip_white_space("   1"), Ok("1"));
+        assert_neq_r!(skip_white_space("   1"), Err(_));
+    }
+
+    #[test]
+    fn test_null_parse() {
+        assert_eq_r!(
+            parse_for_null("null"),
+            Ok(PareseResult {
+                json: "",
+                json_struct: JsonStruct::Null
+            })
+        );
+        assert_eq_r!(parse_for_null("   "), Err(ParseError::ValueError));
+        assert_eq_r!(parse_for_null("null  "), Ok(_));
+        assert_eq_r!(parse_for_null("nul  "), Err(_));
+        assert_eq_r!(parse_for_null("nul"), Err(_));
+        assert_neq_r!(
+            parse_for_null("null"),
+            Ok(PareseResult {
+                json: "  ",
+                json_struct: JsonStruct::Null
+            })
+        );
+        assert_eq_r!(parse_for_null("  null  "), Err(_));
+    }
+
+    #[test]
+    fn test_false_parse() {
+        assert_eq_r!(
+            parse_for_false("false"),
+            Ok(PareseResult {
+                json: "",
+                json_struct: JsonStruct::Boolean(false)
+            })
+        );
+        assert_eq_r!(parse_for_false("   "), Err(ParseError::ValueError));
+        assert_eq_r!(
+            parse_for_false("false  "),
+            Ok(PareseResult {
+                json: "  ",
+                json_struct: JsonStruct::Boolean(false)
+            })
+        );
+        assert_eq_r!(parse_for_false("fal  "), Err(_));
+        assert_eq_r!(parse_for_false("fa"), Err(_));
+        assert_neq_r!(
+            parse_for_false("false"),
+            Ok(PareseResult {
+                json: "",
+                json_struct: JsonStruct::Boolean(true)
+            })
+        );
+        assert_eq_r!(parse_for_false("  false  "), Err(_));
+        assert_eq_r!(parse_for_false("abcd"), Err(_));
+    }
+
+    #[test]
+    fn test_true_parse() {
+        assert_eq_r!(
+            parse_for_true("true"),
+            Ok(PareseResult {
+                json: "",
+                json_struct: JsonStruct::Boolean(true)
+            })
+        );
+        assert_eq_r!(parse_for_true("   "), Err(ParseError::ValueError));
+        assert_eq_r!(parse_for_true("true  "), Ok(_));
+        assert_eq_r!(parse_for_true("tru  "), Err(_));
+        assert_eq_r!(parse_for_true("tru"), Err(_));
+        assert_neq_r!(
+            parse_for_true("true"),
+            Ok(PareseResult {
+                json: " ",
+                json_struct: JsonStruct::Boolean(true)
+            })
+        );
+        assert_eq_r!(parse_for_true("  true  "), Err(_));
+        assert_eq_r!(parse_for_true("abcd"), Err(_));
+    }
+
+    #[test]
+    fn test_num_parse() {
+        assert_eq_n_r!(parse_for_number("10000"), 10000.0, "");
+
+        assert_eq_n_r!(parse_for_number("0.0"), 0.0, "");
+        assert_eq_n_r!(parse_for_number("0.0"), -0.0, "");
+        assert_eq_n_r!(parse_for_number("0.0"), -0.0, "");
+        assert_eq_n_r!(parse_for_number("1.0"), 1.0, "");
+        assert_eq_n_r!(parse_for_number("-1.0"), -1.0, "");
+        assert_eq_n_r!(parse_for_number("1.5"), 1.5, "");
+        assert_eq_n_r!(parse_for_number("-1.5"), -1.5, "");
+        assert_eq_n_r!(parse_for_number("3.1416"), 3.1416, "");
+        assert_eq_n_r!(parse_for_number("1E10"), 1E10, "");
+        assert_eq_n_r!(parse_for_number("1e10"), 1e10, "");
+        assert_eq_n_r!(parse_for_number("1E+10"), 1E+10, "");
+        assert_eq_n_r!(parse_for_number("1E-10"), 1E-10, "");
+        assert_eq_n_r!(parse_for_number("-1E10"), -1E10, "");
+        assert_eq_n_r!(parse_for_number("-1e10"), -1e10, "");
+        assert_eq_n_r!(parse_for_number("-1E+10"), -1E+10, "");
+        assert_eq_n_r!(parse_for_number("-1E-10"), -1E-10, "");
+        assert_eq_n_r!(parse_for_number("1.234E+10"), 1.234E+10, "");
+        assert_eq_n_r!(parse_for_number("1.234E-10"), 1.234E-10, "");
+        assert_eq_n_r!(parse_for_number("0.0"), 1e-10000, "");
+    }
+}
---
+++ src/rjson.rs
@@ -0,0 +1,118 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum ParseError {
+    ValueError,
+}
+
+#[derive(Debug)]
+pub enum JsonStruct {
+    Null,
+    Boolean(bool),
+    Str(String),
+    Number(f32),
+    Array(Vec<JsonStruct>),
+    Object(HashMap<String, JsonStruct>),
+}
+
+#[derive(Debug)]
+pub struct PareseResult<'a> {
+    pub json: &'a str,
+    pub json_struct: JsonStruct,
+}
+
+pub fn parse(json: &str) -> Result<JsonStruct, ParseError> {
+    return if json.len() <= 0 {
+        Ok(JsonStruct::Null)
+    } else if let Ok(json) = skip_white_space(json) {
+        let result = match &json[..1] {
+            "n" => parse_for_null(json),
+            "f" => parse_for_false(json),
+            "t" => parse_for_true(json),
+            _ => Err(ParseError::ValueError),
+        };
+
+        match result {
+            Ok(PareseResult { json, json_struct }) => Ok(json_struct),
+            Err(e) => Err(e),
+        }
+    } else {
+        Ok(JsonStruct::Null)
+    };
+}
+
+pub fn parse_for_true(json: &str) -> Result<PareseResult, ParseError> {
+    return if json.len() < 4 {
+        Err(ParseError::ValueError)
+    } else if &json[0..1] == "t" && &json[1..2] == "r" && &json[2..3] == "u" && &json[3..4] == "e" {
+        Ok(PareseResult {
+            json: &json[4..json.len()],
+            json_struct: JsonStruct::Boolean(true),
+        })
+    } else {
+        Err(ParseError::ValueError)
+    };
+}
+
+pub fn parse_for_false(json: &str) -> Result<PareseResult, ParseError> {
+    return if json.len() < 5 {
+        Err(ParseError::ValueError)
+    } else if &json[0..1] == "f"
+        && &json[1..2] == "a"
+        && &json[2..3] == "l"
+        && &json[3..4] == "s"
+        && &json[4..5] == "e"
+    {
+        Ok(PareseResult {
+            json: &json[5..json.len()],
+            json_struct: JsonStruct::Boolean(false),
+        })
+    } else {
+        Err(ParseError::ValueError)
+    };
+}
+
+pub fn parse_for_null(json: &str) -> Result<PareseResult, ParseError> {
+    return if json.len() < 4 {
+        Err(ParseError::ValueError)
+    } else if &json[0..1] == "n" && &json[1..2] == "u" && &json[2..3] == "l" && &json[3..4] == "l" {
+        Ok(PareseResult {
+            json: &json[4..json.len()],
+            json_struct: JsonStruct::Null,
+        })
+    } else {
+        Err(ParseError::ValueError)
+    };
+}
+
+pub fn parse_for_number(json: &str) -> Result<PareseResult, ParseError> {
+    for (i, &item) in json.as_bytes().iter().enumerate() {
+        let end = if item == b' ' || item == b'\t' || item == b'\n' || item == b'\r' {
+            i
+        } else if i == (json.len() - 1) {
+            json.len()
+        } else {
+            continue;
+        };
+
+        return match &json[..end].parse::<f32>() {
+            Ok(num) => Ok(PareseResult {
+                json: &json[end..],
+                json_struct: JsonStruct::Number(*num),
+            }),
+
+            Err(_) => Err(ParseError::ValueError),
+        };
+    }
+    return Err(ParseError::ValueError);
+}
+
+pub fn skip_white_space(json: &str) -> Result<&str, ParseError> {
+    for (i, &item) in json.as_bytes().iter().enumerate() {
+        if item != b' ' && item != b'\t' && item != b'\n' && item != b'\r' {
+            return Ok(&json[i..]);
+        }
+    }
+
+    return Ok(&json[json.len()..]);
+}
